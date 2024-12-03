31d317529b99f456b3dbc94967999f1954b51cdf
deolv12 <deanandameuthia@gmail.com>
2024-12-03 14:04:03 +0700

    Add files via upload

---
+++ call.py
@@ -0,0 +1,23 @@
+import bangunDatar, bangunRuang, hitung
+
+bangunDatar.l_persegi(7)
+bangunDatar.l_persegi_panjang(12,5)
+bangunDatar.l_segitiga(6,8)
+bangunDatar.l_lingkaran(1)
+
+print()
+
+
+bangunRuang.l_balok(2,4,6)
+bangunRuang.l_prismasegitiga(3,9,5)
+bangunRuang.l_limas(9,5)
+bangunRuang.l_tabung(3,8)
+bangunRuang.l_kubus(4)
+
+print()
+
+hitung.tambah(21,15)
+hitung.kurang(93,17)
+hitung.kali(9,9)
+hitung.bagi(144,6)
+hitung.pangkat(8,9)
